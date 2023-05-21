96447c2849004208482219883c4931a04246bc0a
Alejandro <ThinkRamen@gmail.com>
2023-05-20 20:05:59 -0700

    static files again...

--- eco_tech_mac_backend/settings.py
+++ eco_tech_mac_backend/settings.py
@@ -131,8 +131,9 @@ STATIC_URL = 'static/'
 MEDIA_URL = '/images/'
 
 STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = BASE_DIR / 'staticfiles_build'
 
-MEDIA_ROOT = 'static/images'
+MEDIA_ROOT = BASE_DIR / 'static/images'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
