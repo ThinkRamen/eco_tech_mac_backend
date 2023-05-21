b74c13f3baa0e61b3985f95774b10b684c10057f
Alejandro <ThinkRamen@gmail.com>
2023-05-20 20:03:53 -0700

    media and static

--- eco_tech_mac_backend/settings.py
+++ eco_tech_mac_backend/settings.py
@@ -127,9 +127,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), '/vercel/path0/static']
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+STATIC_URL = 'static/'
+MEDIA_URL = '/images/'
+
+STATICFILES_DIRS = [BASE_DIR / 'static']
+
+MEDIA_ROOT = 'static/images'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
